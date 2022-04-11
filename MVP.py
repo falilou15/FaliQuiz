7e3cead7f0613f1760472d7878207de999f495ee
Falilou <90579427+falilou15@users.noreply.github.com>
2022-04-11 09:27:09 +0200

    Update MVP.py

--- MVP.py
+++ MVP.py
@@ -1,9 +1,13 @@
 #Minimum Viable Product
 
-Def question :
- """Prend en paramettre un int et renvoie selon la réponse si c'est True ou False
+Def question() :
+ """ Prend en paramettre un int et renvoie selon la réponse si c'est True ou False
  """
 
-Def gagne :
- """ qui renvoie un True si on atteint plus de 5 bonnes réponses
+Def gagne() :
+ """ Prend en parametre un int qui renvoie un True si on atteint 5 bonnes réponses sur 5 questions
+ """
+
+ Def perd() :
+ """ Prend en parametre un int qui renvoie un True si on atteint pas au moins 5 bonnes réponses sur 5 questions
  """
