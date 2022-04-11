4be46313dcb0d94c2d362dff41efece58a55c17e
Falilou <90579427+falilou15@users.noreply.github.com>
2022-04-11 09:27:36 +0200

    Update MVP.py

--- MVP.py
+++ MVP.py
@@ -8,6 +8,6 @@ Def gagne() :
  """ Prend en parametre un int qui renvoie un True si on atteint 5 bonnes réponses sur 5 questions
  """
 
- Def perd() :
+Def perd() :
  """ Prend en parametre un int qui renvoie un True si on atteint pas au moins 5 bonnes réponses sur 5 questions
  """
