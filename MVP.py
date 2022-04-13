727fd57f1dc910d77113f5972ce3c8da25e67e6e
Falilou <90579427+falilou15@users.noreply.github.com>
2022-04-13 11:14:36 +0200

    Update MVP.py

--- MVP.py
+++ MVP.py
@@ -1,13 +1,13 @@
 #Minimum Viable Product pour le quiz
 
-Def question(x:int) -> Bool :
- """ Prend en paramettre un int et renvoie selon la réponse si c'est True ou False
+Def question(quest_1 : dict) -> Bool :
+ """ Prend en paramettre un dictionnaire et renvoie selon la réponse si c'est True ou False
  """
 
-Def gagne(x:int) -> Bool :
- """ Prend en parametre un int qui renvoie un True si on atteint 5 bonnes réponses sur 5 questions
+Def gagne(x : int) -> Bool :
+ """ Prend en parametre un entier  qui renvoie un True si on atteint 5 bonnes réponses sur 5 questions
  """
 
-Def perd(x:int) -> Bool :
- """ Prend en parametre un int qui renvoie un True si on atteint pas au moins 5 bonnes réponses sur 5 questions
+Def perd(x : int) -> Bool :
+ """ Prend en parametre un entier qui renvoie un True si on atteint pas au moins 5 bonnes réponses sur 5 questions
  """
