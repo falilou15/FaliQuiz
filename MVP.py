ee9cd272e1a7e9d28ba49b5f8c6252fe81b5adcb
Falilou <90579427+falilou15@users.noreply.github.com>
2022-04-15 15:56:42 +0200

    Update MVP.py

--- MVP.py
+++ MVP.py
@@ -10,7 +10,7 @@ Q1 = {"énoncé" : "Quel arme utilise Thor pour se battre ?" ,
       "bonne-réponse" : 1}
 
 Q2 = {"énoncé" : "Quel est le nom du village ou vit Naruto ?" ,
-      "réponses" : ["1 - Kiri","2 - Konoha","3 - Kumo","4 - Suna"] ,
+      "réponses" : ["1 - Grigny","2 - Konoha","3 - Sarcelles","4 - Kumo"] ,
       "bonne-réponse" : 2}
 
 Q3 = {"énoncé" : "Quel est le titre du premier Star Wars ?" ,
@@ -21,89 +21,32 @@ Q4 = {"énoncé" : "Quel est premier membre de l'équipage de Luffy dans One Pie
       "réponses" : ["1 - Usopp","2 - Nami","3 - Zoro","4 - Sanji"] ,
       "bonne-réponse" : 3}
 
+# Proposition: Mettre les dictionnaires de questions sans un fichier JSOn ou txt
+# Lire ce fichier pour alimenter la liste des questions
+# Faire une interface graphique ?
+# Mémoriser les pseudos/score dans un fichier externe voire un base de données (NSIT_12e)
+
 
 questions = [Q0,Q1,Q2,Q3,Q4]
 
 #question 0
 
 score = 0
-SC = print ("Votre Score est de", score)
-print(questions[0]["énoncé"])
-for r in questions[0]["réponses"]:
-    print(r)   
-réponse_joueur = int(input("Quel est votre réponse ? (répondez en écrivant 1 , 2 , 3 ou 4) "))
-if réponse_joueur == questions[0]["bonne-réponse"]:
-    score += 1
-    print()
-    print("Bonne Réponse ! Votre Score est de", score)
-    print()
-else:
-    print()
-    print("Mauvaise réponse")
-    print()
 
-#question 1
-
-print(questions[1]["énoncé"])
-for r in questions[1]["réponses"]:
-    print(r)
-réponse_joueur = int(input("Quel est votre réponse ? (répondez en écrivant 1 , 2 , 3 ou 4) "))
-if réponse_joueur == questions[1]["bonne-réponse"]:
-    score += 1
-    print()
-    print("Bonne Réponse ! Votre Score est de", score)
-    print()
-else:
-    print()
-    print("Mauvaise réponse")
-    print()
-  
-#question 2
-      
-print(questions[2]["énoncé"])
-for r in questions[2]["réponses"]:
-    print(r)
-réponse_joueur = int(input("Quel est votre réponse ? (répondez en écrivant 1 , 2 , 3 ou 4) "))
-if réponse_joueur == questions[2]["bonne-réponse"]:
-    score += 1
-    print()
-    print("Bonne Réponse ! Votre Score est de", score)
-    print()
-else:
-    print()
-    print("Mauvaise réponse")
-    print()
-    
-#question 3
-      
-print(questions[3]["énoncé"])
-for r in questions[3]["réponses"]:
-    print(r)
-réponse_joueur = int(input("Quel est votre réponse ? (répondez en écrivant 1 , 2 , 3 ou 4) "))
-if réponse_joueur == questions[3]["bonne-réponse"]:
-    score += 1
-    print()
-    print("Bonne Réponse ! Votre Score est de", score)
-    print()
-else:
-    print()
-    print("Mauvaise réponse")
-    print()
-    
-#question 3
-      
-print(questions[4]["énoncé"])
-for r in questions[4]["réponses"]:
-    print(r)
-réponse_joueur = int(input("Quel est votre réponse ? (répondez en écrivant 1 , 2 , 3 ou 4) "))
-if réponse_joueur == questions[4]["bonne-réponse"]:
-    score += 1
-    print()
-    print("Bonne Réponse ! Votre Score est de", score)
-    print()
-else:
-    print()
-    print("Mauvaise réponse")
-    print()
-    
+for q in range(len(questions)):
+    SC = print ("Votre Score est de", score)
+    print(questions[q]["énoncé"])
+    for r in questions[q]["réponses"]:
+        print(r)   
+    réponse_joueur = int(input("Quel est votre réponse ? (répondez en écrivant 1 , 2 , 3 ou 4) "))
+    if réponse_joueur == questions[q]["bonne-réponse"]:
+        score += 1
+        print()
+        print("Bonne Réponse ! Votre Score est de", score)
+        print()
+    else:
+        print()
+        print("Mauvaise réponse")
+        print()
+   
 print("Votre score final est de",score, "Merci d'avoir joué !")
